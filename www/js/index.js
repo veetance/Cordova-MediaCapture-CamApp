0138bf463b5526ebea30989608e9413d69b77333
veetance <divineokos@hotmail.com>
2022-11-22 09:24:47 -0700

    Final-commit - all components as wished :)

--- www/js/index.js
+++ www/js/index.js
@@ -1,31 +1,3 @@
-// make sure .CamcaptureData and .shell  hight is the same as their width of the image optimised for mobile.. for some reason in the css it is not working whan i use aspect-ratio: 1/1; 
-
-$('.shell_span2').css('height', $('.shell_span2').width());
-$('.shell_span1').css('height', $('.shell_span1').width());
-$('.CamcaptureData').css('height', $('.CamcaptureData').width());
-
-
-// function ExpandIMG, on the touch or press of .info-button the hight of CamcaptureData is reduced by 50% with smooth animation and smooth scroll, if the hight is 50% the hight is set to 100% with smooth animation and smooth scroll 
-
-function ExpandIMG() {
-
-    if ($('.CamcaptureData').height() == $('.CamcaptureData').width()) {
-        $('.CamcaptureData').animate({ height: $('.CamcaptureData').width() / 2 }, 400, 'swing');
-        $('html, body').animate({ scrollTop: $('.CamcaptureData').offset().top }, 450, 'swing');
-    } else {
-        $('.CamcaptureData').animate({ height: $('.CamcaptureData').width() }, 500, 'swing');
-        $('html, body').animate({ scrollTop: $('.CamcaptureData').offset().top }, 500, 'swing');
-    }
-}
-
-// call the function on the touch or press of .info-button
-
-$('.info-button').on('click', ExpandIMG);
-
-
-
-
-
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
@@ -47,6 +19,7 @@ function onDeviceReady() {
 
     function onSuccess(imageData) {
         console.log(imageData);
+        $(".instructions").hide();
         resolveLocalFileSystemURL(imageData, function (fileEntry) {
             var myNewImage = fileEntry.toURL()
             console.log(myNewImage);
@@ -66,7 +39,93 @@ function onDeviceReady() {
 
 };
 
-// 
+
+
+
+// .CamcaptureData and .shell hight is the same as its width - **for some reason in the css it is not working whn i used aspect-ratio: 1/1;** 
+
+$('.shell_span2').css('height', $('.shell_span2').width());
+$('.shell_span1').css('height', $('.shell_span1').width());
+$('.CamcaptureData').css('height', $('.CamcaptureData').width());
+
+
+// the hight of CamcaptureData is reduced by 50%, if the hight is 50%, the hight is set to 100%  
+
+function ExpandIMG() {
+    
+
+    if ($('.CamcaptureData').height() == $('.CamcaptureData').width()) {
+        $('.CamcaptureData').animate({ height: $('.CamcaptureData').width() / 2 }, 400, 'swing');
+        $('html, body').animate({ scrollTop: $('.CamcaptureData').offset().top }, 450, 'swing');
+    } else {
+        $('.CamcaptureData').animate({ height: $('.CamcaptureData').width() }, 400, 'swing');
+        $('html, body').animate({ scrollTop: $('.CamcaptureData').offset().top }, 450, 'swing');
+    }
+}
+
+$('.info-button').on('click', ExpandIMG);
+
+
+//rotate animation toggle,.info-button is touched or pressed the class .rotate is added to .arrow_up and when .info-button is touched or pressed again the class .rotate is removed from .info-button.
+
+
+function rotateArrow() {
+
+    $('.arrow_up').toggleClass('rotate');
+}
+
+$('.info-button').on('click', rotateArrow);
+
+
+// array of maximum 72 imgs, grab the images from the img folder and add them to array GalleryImages
+
+var GalleryImages = [];
+for (var i = 1; i < 73; i++) {
+    GalleryImages.push('img/Galleree/art-0' + i + '.png');
+}
+
+// console.log(GalleryImages);
+
+// function populateGallery is called when the page is loaded, it loops through the array GalleryImages and adds random 12 images to .shell background
+
+function populateGallery() {
+
+    for (var i = 0; i < 12; i++) {
+        var random = Math.floor(Math.random() * GalleryImages.length);
+        $('.shell').eq(i).css('background-image', 'url(' + GalleryImages[random] + ')');
+        GalleryImages.splice(random, 1);
+    }
+}
+
+$(document).ready(function () {
+    populateGallery();
+});
+
+
+$('.nav-bar_logo').on('click', function () {
+    location.reload();
+});
+
+
+// the background image url of .CamcaptureData is changed to the background-img url of the .shell that was clicked
+
+$('.shell').on('click', function () {
+    $('.CamcaptureData').css('background-image', $(this).css('background-image'));
+
+    if ($('.CamcaptureData').height() == $('.CamcaptureData').width() / 2) {
+        $('.CamcaptureData').css('height', $('.CamcaptureData').width());
+    }
+
+    $(".instructions").hide();
+});
+
+
+
+
+
+
+
+
 
 
 
